78fbf8a4f95f4a8652486bf939e15cb48821e933
Anjana R <44143225+AnjanaRajanM@users.noreply.github.com>
2019-07-31 06:38:12 -0700

    Not binding

--- Invento19.Android/MainActivity.cs
+++ Invento19.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Platform;
+using Xamarin.Forms;
 
 namespace Invento19.Droid
 {
@@ -18,9 +20,11 @@ namespace Invento19.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            Forms.SetFlags("CollectionView_Experimental");
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CachedImageRenderer.Init(true);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ Invento19/Events/Eventpage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Invento19.Events
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+   
+    public partial class Eventpage : ContentPage
+    {
+        public Eventpage()
+        {
+            InitializeComponent();
+         
+            List<string> Workshops = new List<string>
+            {
+                "webdevelopment.svg","webdevelopment.svg","webdevelopment.svg", "webdevelopment.svg","webdevelopment.svg","webdevelopment.svg"
+            };
+            FirstCollectionView.ItemsSource = Workshops;
+            List<string> Talks = new List<string>
+            {
+               "webdevelopment.svg","webdevelopment.svg","webdevelopment.svg", "webdevelopment.svg","webdevelopment.svg","webdevelopment.svg"
+            };
+            SecondCollectionView.ItemsSource = Talks;
+            List<string> Comp = new List<string>
+            {
+                "coderelay.svg","dbmania.svg","inspecta.svg","coderelay.svg","dbmania.svg","inspecta.svg"
+
+            };
+            List<string> Names = new List<string>
+            {
+                "code","dbmania","inspecta","coderelay","dbmania","inspecta"
+
+            };
+            List<string> Abouts = new List<string>
+            {
+                "dbmania","inspecta","coderelay","dbmania","inspecta","coderelay"
+
+            };
+            CompCollectionView.ItemsSource = Comp;
+            CompCollectionView.ItemsSource = Names;
+            CompCollectionView.ItemsSource = Abouts;
+            List<string> Competition = new List<string>
+            {
+                "coderelay.svg","dbmania.svg","inspecta.svg","coderelay.svg","dbmania.svg","inspecta.svg"
+
+
+
+            };
+            MainCollectionView.ItemsSource = Competition;
+            List<string> Name = new List<string>
+            {
+                "code","dbmania","inspecta","coderelay","dbmania","inspecta"
+            };
+            List<string> About = new List<string>
+            {
+               "dbmania","inspecta","coderelay","dbmania","inspecta","coderelay"
+            };
+            MainCollectionView.ItemsSource = Name;
+           
+            MainCollectionView.ItemsSource = About;
+          
+        }
+        
+    }
+}
